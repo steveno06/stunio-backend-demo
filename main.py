49947fb95770b44e2b60e3a0a5f7e2f2f4bacdea
steveno06 <stevenohrdorf@gmail.com>
2024-12-17 11:03:58 +0100

    feat: Initialized sqlite database

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from routers import user_routes
+from sqlalchemy.orm import Session
+from database.config import get_db
 app = FastAPI()
 
 @app.get("/")
-def test_root():
+def test_root(db: Session = Depends(get_db)):
     return {"hello":"hello"}
 
 app.include_router(user_routes.router)
