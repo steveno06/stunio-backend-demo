f1d523d2b27c1170ad2edc03126b130217ea73ba
steveno06 <stevenohrdorf@gmail.com>
2024-12-17 17:12:33 +0100

    fix: Updated base user attributes

--- models/user_model.py
+++ models/user_model.py
@@ -1,5 +1,10 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Enum
 from database.config import Base
+import enum
+
+class UserType(enum.Enum):
+    STUDENT = "student"
+    BUSINESS = "business"
 
 class User(Base):
     __tablename__ = "users"
@@ -7,3 +12,5 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique=True, index=True)
     password = Column(String)
+    email = Column(String, unique=True)
+    user_type = Column(Enum(UserType))
