079be71a30737309705a0b8f49368cbc15f53a5e
steveno06 <stevenohrdorf@gmail.com>
2024-12-19 19:00:08 +0100

    fix: Updated to corrent Boolean data type

--- models/job_model.py
+++ models/job_model.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Enum, ForeignKey, Date, DateTime
+from sqlalchemy import Column, Integer, String, Enum, ForeignKey, Date, DateTime, Boolean
 from sqlalchemy.orm import relationship
 from database.config import Base
 from datetime import datetime
@@ -12,10 +12,10 @@ class Job(Base):
     description = Column(String)
     required_students = Column(Integer, nullable=False)
     event_date = Column(Date,nullable=False)
-    is_booked = Column(bool, default=False)
+    is_booked = Column(Boolean, default=False)
     created_at = Column(DateTime, default=datetime.utcnow)
 
-    business = relationship("User", backref="business_profile")
+    business = relationship("User", backref="host_business")
 
 class JobInvite(Base):
     __tablename__ = "job_invites"
@@ -23,7 +23,7 @@ class JobInvite(Base):
     id = Column(Integer, primary_key=True, index=True)
     job_id = Column(Integer, ForeignKey("jobs.id"), index=True)
     student_id = Column(Integer, ForeignKey("users.id"), index=True)
-    has_accepted = Column(bool, default=False)
+    has_accepted = Column(Boolean, default=False)
 
     job = relationship("Job", backref="invites")
     student = relationship("User", backref='job_invites')
