722c100690c57193337a63272ba2a0a3ffad7218
steveno06 <stevenohrdorf@gmail.com>
2024-12-19 18:00:48 +0100

    feat: Job invite model created

--- models/job_model.py
+++ models/job_model.py
@@ -7,7 +7,7 @@ class Job(Base):
     __tablename__ = "jobs"
 
     id = Column(Integer, primary_key=True, index=True)
-    business_id = Column(Integer, ForeignKey("users.id"))
+    business_id = Column(Integer, ForeignKey("users.id"), index=True)
     title = Column(String, nullable=False)
     description = Column(String)
     required_students = Column(Integer, nullable=False)
@@ -16,3 +16,15 @@ class Job(Base):
     created_at = Column(DateTime, default=datetime.utcnow)
 
     business = relationship("User", backref="business_profile")
+
+class JobInvite(Base):
+    __tablename__ = "job_invites"
+
+    id = Column(Integer, primary_key=True, index=True)
+    job_id = Column(Integer, ForeignKey("jobs.id"), index=True)
+    student_id = Column(Integer, ForeignKey("users.id"), index=True)
+    has_accepted = Column(bool, default=False)
+
+    job = relationship("Job", backref="invites")
+    student = relationship("User", backref='job_invites')
+    
